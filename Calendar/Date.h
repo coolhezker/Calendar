e9544a56fcacdfb41f3941208a1a910aba000893
coolhezker <coolhezker@shalqi.com>
2024-11-18 22:06:39 +0300

    Сделал более конкретные и удобные поля для класса Date. Из-за этого выросли классы Day и Month. Ну и определил нужные методы

--- Calendar/Date.h
+++ Calendar/Date.h
@@ -15,7 +15,10 @@ private:
 	int Hour;
 	int Minute;
 public:
-
+	Day();
+	explicit Day(int N, int H, int M);
+	bool setDay(int N, int H, int M);
+	bool checkDay(Day D);
 };
 
 class Date {
@@ -24,7 +27,10 @@ private:
 	Month MonthOD; // Month of Date
 	Day DayOD; // Day of Date
 public:
-
+	Date();
+	explicit Date(int Y, Month M, Day D);
+	bool setDate(int Y, Month M, Day D);
+	bool checkDate(Date D);
 };
 
 #endif // !DATE_H
