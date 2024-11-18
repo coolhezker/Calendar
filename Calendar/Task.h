4b61e3e5779d3a5aec4f5de2ad556835fa31248c
coolhezker <coolhezker@shalqi.com>
2024-11-18 22:08:06 +0300

    Решил поупражняться с обобщённым программированием, шаблонами и концептами(которые ввели в последнем стандарте C++20). Также определил окончательные поля и методы. Пока мне всё нравится

--- Calendar/Task.h
+++ Calendar/Task.h
@@ -6,16 +6,48 @@
 
 using namespace std;
 
+namespace Concepts {
+	template<typename T>
+	concept OStream = requires(T t, ostream & os) {
+		os << t;
+	};
+
+	template<typename T>
+	concept IStream = requires(T t, istream & is) {
+		is >> t;
+	};
+
+	template<typename T>
+	concept Streams = IStream<T> && OStream<T>;
+
+	template<typename T>
+	concept CConstruct = requires(T t) { // check CopyConstruct
+		T(t);
+	};
+
+	template<typename T>
+	concept CAssign = requires(T t1, T t2) {
+		t1 = t2;
+	};
+
+	template<typename T>
+	concept CopyCA = CAssign<T> && CConstruct<T>;
+
+	template<typename T>
+	concept General = CopyCA<T> && Streams<T>;
+}
+
+template<Concepts::General T>
 class Task {
 protected:
-	int Id;
 	string Name;
-	string Content;
-	string Path;
+	T Content;
 	Date DateOT; // Date of Task
 public:
-	bool setContent(const string& cont);
+	bool setContent(const T& cont);
 	bool setDate(Date d);
+	bool setName(string n);
+	void printTask();
 };
 
 #endif // !TASK_H
