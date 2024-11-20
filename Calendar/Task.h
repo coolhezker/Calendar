6410162578960fb2649fe6e2d50d24ec3a0f025c
coolhezker <coolhezker@shalqi.com>
2024-11-20 03:06:35 +0300

    Тут тоже, надеюсь, конечные поля и методы.

--- Calendar/Task.h
+++ Calendar/Task.h
@@ -3,51 +3,21 @@
 
 #include <string>
 #include "Date.h"
-
-using namespace std;
-
-namespace Concepts {
-	template<typename T>
-	concept OStream = requires(T t, ostream & os) {
-		os << t;
-	};
-
-	template<typename T>
-	concept IStream = requires(T t, istream & is) {
-		is >> t;
-	};
-
-	template<typename T>
-	concept Streams = IStream<T> && OStream<T>;
-
-	template<typename T>
-	concept CConstruct = requires(T t) { // check CopyConstruct
-		T(t);
-	};
-
-	template<typename T>
-	concept CAssign = requires(T t1, T t2) {
-		t1 = t2;
-	};
-
-	template<typename T>
-	concept CopyCA = CAssign<T> && CConstruct<T>;
-
-	template<typename T>
-	concept General = CopyCA<T> && Streams<T>;
-}
+#include "Concepts.h"
 
 template<Concepts::General T>
 class Task {
 protected:
-	string Name;
-	T Content;
+	std::string Name; // Name of task
+	T Content; // Content of task
 	Date DateOT; // Date of Task
 public:
-	bool setContent(const T& cont);
-	bool setDate(Date d);
-	bool setName(string n);
-	void printTask();
+	Task(); // standart Constructor
+	explicit Task(std::string N, T C, Date D);
+	virtual void setContent(const T& cont);
+	void setDate(Date D);
+	void setName(std::string N);
+	virtual void printTask();
 };
 
 #endif // !TASK_H
