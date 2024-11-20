37675042d3191682bcf228fbec9f0f209a1bd091
coolhezker <coolhezker@shalqi.com>
2024-11-20 03:05:48 +0300

    Вроде бы, конечные объявления полей и методов(надеюсь)

--- Calendar/Date.h
+++ Calendar/Date.h
@@ -1,6 +1,8 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <string>
+
 enum class Month {
 	January = 1, February,
 	March, April, May,
@@ -10,27 +12,36 @@ enum class Month {
 };
 
 class Day {
-private:
-	int NumberOD; // Number of Day
+protected:
+	int NumberOD;
 	int Hour;
 	int Minute;
+	bool isValidDay(int H, int M, int N);
 public:
 	Day();
-	explicit Day(int N, int H, int M);
-	bool setDay(int N, int H, int M);
-	bool checkDay(Day D);
+	explicit Day(int H, int M, int N);
+	bool setDay(int H, int M, int N);
+	Day getDay();
+	int getNOD() const; // get Number Of Day
+	virtual std::string stringDay();
 };
 
 class Date {
-private:
+protected:
 	int Year;
 	Month MonthOD; // Month of Date
 	Day DayOD; // Day of Date
+	bool isLeapYear(int Y);
+	int CountOfDays(int Y, Month M);
+	bool isValidYear(int Y);
+	bool isValidDate(int Y, Month M, Day D);
 public:
 	Date();
 	explicit Date(int Y, Month M, Day D);
 	bool setDate(int Y, Month M, Day D);
-	bool checkDate(Date D);
+	virtual std::string stringDay();
+	virtual std::string stringMonth();
+	virtual void printDate();
 };
 
 #endif // !DATE_H
